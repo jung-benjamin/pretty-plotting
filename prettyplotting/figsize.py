d46db67f226d837adac3285c09919789a1bba6d2
Benjamin Jung <jung-benjamin@netcologne.de>
2021-07-30 11:25:38 +0200

    Change ratio setting in figsize.set_size function
    The function argument to set the ratio between
    height and width of the plots is changed from a
    bool with a fixed option to variable the accepts
    float or string

--- prettyplotting/figsize.py
+++ prettyplotting/figsize.py
@@ -2,26 +2,34 @@
 
 """Adjust the figsize to LaTeX"""
 
-def set_size(width, fraction=1, subplots=(1, 1), higher=False):
+def set_size(width, fraction=1, subplots=(1, 1), ratio='golden'):
     """Set figure dimensions to avoid scaling in LaTeX.
 
     Parameters
     ----------
-    width: float or string referring to a documentclass
-            Document textwidth or columnwidth in pts
-            Use \the\columnwidth or \the\textwidth in the 
-            LaTeX document to find the correct width
-    fraction: float, optional
-            Fraction of the width which you wish the figure to occupy
-    subplots: tuple of two integers, optional 
-            Configuration of subplots in the figure
-    higher: bool, optional
-            rescale the figure to be longer in the y-axis
+    width
+        float or string referring to a documentclass
+        Document textwidth or columnwidth in pts
+        Use \the\columnwidth or \the\textwidth in the 
+        LaTeX document to find the correct width
+    
+    fraction
+        float, optional
+        Fraction of the width which you wish the figure to occupy
+    
+    subplots 
+        tuple of two integers, optional 
+        Configuration of subplots in the figure
+    
+    ratio 
+        int or string, optional
+        ratio of the x and y directions of the plots
 
     Returns
     -------
-    fig_dim: tuple
-            Dimensions of figure in inches
+    fig_dim
+        tuple
+        Dimensions of figure in inches
     """
     
     if width == 'mythesis':
@@ -45,14 +53,16 @@ def set_size(width, fraction=1, subplots=(1, 1), higher=False):
     # https://disq.us/p/2940ij3
     golden_ratio = (5**.5 - 1) / 2
 
+    if ratio == 'golden':
+        ratio_num = golden_ratio
+    else:
+        ratio_num = ratio
+
     # Figure width in inches
     fig_width_in = fig_width_pt * inches_per_pt
     # Figure height in inches
     
-    if higher:
-        fig_height_in = fig_width_in * golden_ratio * (subplots[0] * 1.3 / subplots[1])
-    else:
-        fig_height_in = fig_width_in * golden_ratio * (subplots[0] / subplots[1])
+    fig_height_in = fig_width_in * ratio_num * (subplots[0] / subplots[1])
     fig_dim = (fig_width_in, fig_height_in)
 
     return fig_dim
