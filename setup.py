1263f3e9ad6109a5c5bd92ecfe4b1b3972ef8751
jung-benjamin <63796898+jung-benjamin@users.noreply.github.com>
2025-10-13 13:17:18 +0200

    Convert install procedure to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,7 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name = 'pretty_plotting',
-    version = '0.1.0',
-    packages = find_packages(include = ['prettyplotting', 'prettyplotting.*'])
-    )
+setup()
