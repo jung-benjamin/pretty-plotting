0f91becc34528c5b5429d9aab2ea20f5fbd56f97
jung-benjamin <63796898+jung-benjamin@users.noreply.github.com>
2025-10-13 13:09:12 +0200

    Convert rwth_colors file to JSON format

--- prettyplotting/colors.py
+++ prettyplotting/colors.py
@@ -1,28 +1,13 @@
 #! /usr/bin/env python3
 
-import re
+import json
 import os
 
-definecolor_regex = re.compile(r"""(
-                                    (\{.+\})
-                                    \s*
-                                    \{RGB\}
-                                    (\{\s*\d{1,3}\s+\d{1,3}\s+\d{1,3}\})
-                                    )""",
-                                    re.VERBOSE)
-
-rwth_colors = {}
-
 current_dir = os.path.dirname(os.path.abspath(__file__))
-with open(os.path.join(current_dir, 'rwth_colors'), 'r') as f:
-    text = f.read()
-    for groups in definecolor_regex.findall(text):
-        name = groups[1].strip('{}')
-        # lstrip is necessary to remove leading whitespace,
-        # otherwise re.split adds an empty string (as per documentation)
-        rgb_str = re.split(r'\s+', groups[2].strip('{}').lstrip())
-        rgb_val = [float(i)/255 for i in rgb_str]
-        rwth_colors[name] = rgb_val
+with open(os.path.join(current_dir, 'rwth_colors.json'), 'r') as f:
+    color_data = json.load(f)
+
+rwth_colors = {n: [i / 255 for i in col] for n, col in color_data.items()}
 
 if __name__ == '__main__':
     for n, col in rwth_colors.items():
