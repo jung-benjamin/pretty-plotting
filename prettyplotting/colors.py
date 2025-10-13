c03814106772d5287faf4016abb52c5ee4283df5
jung-benjamin <63796898+jung-benjamin@users.noreply.github.com>
2025-10-13 13:22:40 +0200

    Update colors to use package-data

--- prettyplotting/colors.py
+++ prettyplotting/colors.py
@@ -1,13 +1,10 @@
 #! /usr/bin/env python3
 
+import importlib.resources as pkg_resources
 import json
-import os
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
-with open(os.path.join(current_dir, 'rwth_colors.json'), 'r') as f:
-    color_data = json.load(f)
-
-rwth_colors = {n: [i / 255 for i in col] for n, col in color_data.items()}
+with pkg_resources.open_text('prettyplotting', 'rwth_colors.json') as f:
+    rwth_colors = {n: [i / 255 for i in col] for n, col in json.load(f).items()}
 
 if __name__ == '__main__':
     for n, col in rwth_colors.items():
